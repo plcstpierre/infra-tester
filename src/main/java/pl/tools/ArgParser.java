154b90866ea4bbde924e6751e383dc3d8a354ceb
Pier-Luc Caron St-Pierre <pierluc.caronstpierre@gmail.com>
2013-10-21 21:30:00 -0400

    Applying apache license

--- src/main/java/pl/tools/ArgParser.java
+++ src/main/java/pl/tools/ArgParser.java
@@ -1,3 +1,21 @@
+/*
+ *
+ *  * (C) Copyright 2013 Pier-Luc Caron St-Pierre
+ *  *
+ *  * Licensed under the Apache License, Version 2.0 (the "License");
+ *  * you may not use this file except in compliance with the License.
+ *  * You may obtain a copy of the License at
+ *  *
+ *  *     http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the License is distributed on an "AS IS" BASIS,
+ *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  * See the License for the specific language governing permissions and
+ *  * limitations under the License.
+ *  *
+ */
+
 package pl.tools;
 
 import java.net.URL;
@@ -13,7 +31,7 @@ public class ArgParser {
     }
 
     public URL parse() {
-        if (args == null || args.length != 1 ){
+        if (args == null || args.length != 1) {
             throw new IllegalArgumentException("Invalid argument count!");
         }
 
